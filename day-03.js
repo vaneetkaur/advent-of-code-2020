374a79c3a52a56970591976ae21bc45ddcb8693c
Vaneet Kaur <vaneet.kaur@tesglobal.com>
2020-12-09 22:31:09 +0000

    Day 3 part 2

--- day-03.js
+++ day-03.js
@@ -24,5 +24,35 @@ const TreesEncountered = () => {
 }
 // 278
 
-TreesEncountered()
-  .then(console.log);
+// TreesEncountered()
+//   .then(console.log);
+
+// What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+
+const TreesEncounteredOnSlope = (data, x, y) => {
+  let currentX = -x;
+  let treeCount = 0;
+
+  for (let i = 0; i < data.length; i = i + y) {
+    currentX = currentX + x;
+    if (currentX >= data[i].length) currentX = currentX - data[i].length;
+    if (data[i][currentX] === '#') ++treeCount;
+  }
+  return treeCount;
+}
+
+const MultiplyTreesEncountered = async () => {
+  const data = await getData('./day-03-data.txt');
+
+  const slope1 = TreesEncounteredOnSlope(data, 1, 1);
+  const slope2 = TreesEncounteredOnSlope(data, 3, 1);
+  const slope3 = TreesEncounteredOnSlope(data, 5, 1);
+  const slope4 = TreesEncounteredOnSlope(data, 7, 1);
+  const slope5 = TreesEncounteredOnSlope(data, 1, 2);
+
+  return slope1 * slope2 * slope3 * slope4 * slope5;
+}
+
+MultiplyTreesEncountered().then(console.log);
+
+// 9709761600
